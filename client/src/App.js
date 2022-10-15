d2f9a669110700c58b787007dbd6ad7d2a791837
Raj Karan Singh <thewackyindian@gmail.com>
2022-10-16 00:36:20 +0530

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
-import { BrowserRouter, Routes, Route , withRouter} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import SignIn from "./pages/SignIn";
